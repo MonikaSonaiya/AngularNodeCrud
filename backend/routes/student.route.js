eb014f621ec0838ba5d2cdd48c2b24b1683012a8
MonikaSonaiya <39123141+MonikaSonaiya@users.noreply.github.com>
2020-01-02 09:50:37 +0530

    add logs

--- backend/routes/student.route.js
+++ backend/routes/student.route.js
@@ -45,8 +45,8 @@ studentRoute.route('/update-student/:id').put((req, res, next) => {
     $set: req.body
   }, (error, data) => {
     if (error) {
+       console.log(error)
       return next(error);
-      console.log(error)
     } else {
       res.json(data)
       console.log('Student successfully updated!')
